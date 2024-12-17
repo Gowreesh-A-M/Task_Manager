4370d09ab20be1b4ba4b316c4e5f64a7d0dec691
Gowreesh-A-M <gowreesh@testpress.in>
2024-12-17 15:59:57 +0530

    to store the tasks in map

---
+++ lib/models/task.dart
@@ -0,0 +1,30 @@
+// lib/models/task.dart
+class Task {
+  String id;
+  String title;
+  bool isCompleted;
+
+  Task({
+    required this.id,
+    required this.title,
+    this.isCompleted = false,
+  });
+
+  // Convert Task to Map for storage
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  // Create Task from Map
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'],
+      title: map['title'],
+      isCompleted: map['isCompleted'],
+    );
+  }
+}
